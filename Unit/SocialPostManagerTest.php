1c00444c23478a8901aa82ef395ec77fc4723e1a
neerajp99 <neerajp1999@gmail.com>
2019-06-17 23:10:16 +0530

    Edit tests for class SocialPostManager

--- Unit/SocialPostManagerTest.php
+++ Unit/SocialPostManagerTest.php
@@ -3,7 +3,6 @@
 use Drupal\social_post\SocialPostManager;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Session\AccountProxy;
-use Drupal\Core\Site\Settings;
 use Drupal\social_post\SocialPostDataHandler;
 use Drupal\Tests\UnitTestCase;
 
@@ -15,21 +14,6 @@ use Drupal\Tests\UnitTestCase;
 class SocialPostManagerTest extends UnitTestCase {
 
   /**
-   * Define __construct function.
-   */
-  public function __construct() {
-    parent::__construct();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setUp() {
-    parent::setUp();
-    $provider_user_id = 'drupaluser';
-  }
-
-  /**
    * Tests for class UserAccessControlHandler.
    */
   public function testSocialPostManager() {
@@ -39,47 +23,51 @@ class SocialPostManagerTest extends UnitTestCase {
     $current_user = $this->createMock(AccountProxy::class);
     $data_handler = $this->createMock(SocialPostDataHandler::class);
     $socialPostManager = $this->getMockBuilder(SocialPostManager::class)
-                              ->setConstructorArgs(array($entity_type_manager,
-                                                         $current_user,
-                                                         $data_handler,
-                                                   ))
-                              ->getMock();
+      ->setConstructorArgs([$entity_type_manager,
+        $current_user,
+        $data_handler,
+      ])
+      ->getMock();
 
     $socialPostManager->setPluginId('drupal123');
+
     $socialPostManager->method('getPluginId')
-                      ->willReturn('drupal123');
+      ->willReturn('drupal123');
 
     $socialPostManager->method('getCurrentUser')
-                      ->willReturn(123);
+      ->willReturn(123);
 
     $socialPostManager->method('getAccountsByUserId')
-                      ->with($plugin_id, $user_id)
-                      ->will($this->returnValue(array(
-                        'user_id' => $user_id,
-                        'plugin_id' => $plugin_id,
-                      )));
+      ->with($plugin_id, $user_id)
+      ->will($this->returnValue([
+        'user_id' => 'drupaluser',
+        'plugin_id' => 'drupal',
+      ]));
 
     $socialPostManager->method('addRecord')
-                      ->with('drupaluser','drupal123', 'drupal', $additional_data = NULL)
-                      ->will($this->returnValue(TRUE));
+      ->with('drupaluser', 'drupal123', 'drupal', $additional_data = NULL)
+      ->will($this->returnValue(TRUE));
 
-    $socialPostManager->setSessionKeysToNullify(array('drupal'));
+    $socialPostManager->setSessionKeysToNullify(['drupal']);
 
     $socialPostManager->method('getSalt')
-                      ->willReturn('drupal3ab9');
+      ->willReturn('drupal3ab9');
 
     $this->assertTrue(
           method_exists($socialPostManager, 'setPluginId'),
             'SocialPostManager does not implements setPluginId function/method'
     );
+
     $this->assertTrue(
           method_exists($socialPostManager, 'getPluginId'),
             'SocialPostManager does not implements getPluginId function/method'
     );
+
     $this->assertTrue(
           method_exists($socialPostManager, 'getAccountsByUserId'),
             'SocialPostManager does not implements getAccountsByUserId function/method'
     );
+
     $this->assertTrue(
           method_exists($socialPostManager, 'checkIfUserExists'),
             'SocialPostManager does not implements checkIfUserExists function/method'
@@ -88,18 +76,22 @@ class SocialPostManagerTest extends UnitTestCase {
           method_exists($socialPostManager, 'getCurrentUser'),
             'SocialPostManager does not implements getCurrentUser function/method'
     );
+
     $this->assertTrue(
           method_exists($socialPostManager, 'addRecord'),
             'SocialPostManager does not implements addRecord function/method'
     );
+
     $this->assertTrue(
           method_exists($socialPostManager, 'setSessionKeysToNullify'),
             'SocialPostManager does not implements setSessionKeysToNullify function/method'
     );
+
     $this->assertTrue(
           method_exists($socialPostManager, 'nullifySessionKeys'),
             'SocialPostManager does not implements cnullifySessionKeys function/method'
     );
+
     $this->assertTrue(
           method_exists($socialPostManager, 'updateToken'),
             'SocialPostManager does not implements updateToken function/method'
@@ -108,6 +100,7 @@ class SocialPostManagerTest extends UnitTestCase {
           method_exists($socialPostManager, 'getToken'),
             'SocialPostManager does not implements getToken function/method'
     );
+
     $this->assertTrue(
           method_exists($socialPostManager, 'encryptToken'),
             'SocialPostManager does not implements encryptToken function/method'
@@ -116,15 +109,21 @@ class SocialPostManagerTest extends UnitTestCase {
           method_exists($socialPostManager, 'decryptToken'),
             'SocialPostManager does not implements decryptToken function/method'
     );
+
     $this->assertTrue(
           method_exists($socialPostManager, 'getSalt'),
             'SocialPostManager does not implements getSalt function/method'
     );
+
     $this->assertEquals('drupal123', $socialPostManager->getPluginId());
     $this->assertEquals(123, $socialPostManager->getCurrentUser());
-    $this->assertEquals(['user_id' => 'drupaluser', 'plugin_id' => 'drupal'],
-                       $socialPostManager->getAccountsByUserId($plugin_id, $user_id));
+    $this->assertEquals([
+      'user_id' => 'drupaluser',
+      'plugin_id' => 'drupal',
+    ],
+    $socialPostManager->getAccountsByUserId($plugin_id, $user_id));
     $this->assertEquals('drupal3ab9', $socialPostManager->getSalt());
-    $this->assertTrue($socialPostManager->addRecord('drupaluser','drupal123', 'drupal', $additional_data = NULL));
+    $this->assertTrue($socialPostManager->addRecord('drupaluser', 'drupal123', 'drupal', $additional_data = NULL));
   }
+
 }
