3501aa2f9390b244ff0e87705ae5832b15086273
neerajp99 <neerajp1999@gmail.com>
2019-06-17 23:09:51 +0530

    Edit tets for PLugin

--- Unit/PluginTest.php
+++ Unit/PluginTest.php
@@ -1,7 +1,6 @@
 <?php
 
 use Drupal\social_post\Plugin\Network\SocialPostNetworkInterface;
-use Drupal\social_api\Plugin\NetworkInterface;
 use Drupal\Tests\UnitTestCase;
 
 /**
@@ -12,25 +11,12 @@ use Drupal\Tests\UnitTestCase;
 class NetworkTest extends UnitTestCase {
 
   /**
-   * Define __construct function.
-   */
-  public function __construct() {
-    parent::__construct();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setUp() {
-    parent::setUp();
-  }
-
-  /**
    * Tests for class Network.
    */
   public function testSocialPostNetworkInterface() {
+
     $socialPostNetworkInterface = $this->getMockBuilder(SocialPostNetworkInterface::class)
-                                       ->getMock();
+      ->getMock();
     $this->assertTrue(
           method_exists($socialPostNetworkInterface, 'post'),
             'SocialPostNetworkInterface does not implements post function/method'
